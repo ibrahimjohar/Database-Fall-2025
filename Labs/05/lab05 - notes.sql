aae765762eaf30ebdecc72a3f2127f93c9aee7e4
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-09-18 08:45:03 +0500

    Create lab05 - notes.sql

---
+++ Labs/05/lab05 - notes.sql
@@ -0,0 +1,15 @@
+select * from students;
+select * from departments;
+insert into students values(1, 'ali khan', 'ali@gmail.com', 21, 60000, 'karachi', 1);
+
+create table faculty(
+id int primary key,
+name varchar(20)
+);
+select * from faculty;
+insert into faculty values(1, 'Miss fareeha');
+insert into faculty values(2, 'miss aqsa');
+insert into faculty values(3, 'miss aisha');
+
+alter table students add (f_id int, foreign key(f_id), REFERENCES faculty(id));
+update students set f_id = 1 where id in(2);
