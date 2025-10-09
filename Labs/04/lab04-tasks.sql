e588851b16c6339b2c4d7168e776c07fd11ea243
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-10-09 05:16:38 +0500

    updated lab04-tasks.sql

--- Labs/04/lab04-tasks.sql
+++ Labs/04/lab04-tasks.sql
@@ -72,8 +72,91 @@ where course_id in (select course_id
                                     )
                     );
 
+--postLab tasks
+
+--q11
+select dept_id, sum(fee) as total_fee
+from students
+group by dept_id
+having sum(fee) > 1000000;
+
+--q12
+select dept_id, count(*) as top_faculty
+from faculty
+where salary > 100000
+group by dept_id
+having count(*) > 5;
+
+--q13
+delete from students
+where gpa < (select avg(gpa) from students);
+
+--q14
+delete from courses
+where course_id not in (select distinct course_id from enrollment);
+
+--q15
+create table HighFee_Students as
+select *
+from students
+where fee > (select avg(fee) from students)
+
+--q16
+create table retired_faculty(
+            faculty_id int primary key,
+            faculty_name varchar2(50),
+            dept_id number,
+            salary number,
+            joining_date date,
+            foreign key (dept_id) references department(dept_id) on delete set null);
+insert into Retired_Faculty
+select * 
+from faculty
+where joining_date < (select min(joining_date) from faculty);
+
+--q17
+select dept_id, sum(fee) as total_fee
+from students
+group by dept_id
+having sum(fee) = (
+        select max(sum(fee))
+        from students
+        group by dept_id
+);
+select dept_id, total_fee
+from (
+    select dept_id, total_fee, rownum as rn
+    from (
+        select dept_id, (sum(fee)) as total_fee
+        from students
+        group by dept_id
+        order by sum(fee) desc
+    )
+)
+where rn = 1;
 
 
+--q18
+select * 
+from (
+    select course_id, count(*) as total_enrolled
+    from enrollment
+    group by course_id
+    order by count(*) desc
+)
+where rownum <= 3;
 
 
+--q19
+select * from students where gpa > (select avg(gpa) from students) 
+and (std_id in (select std_id 
+                from enrollment 
+                group by std_id 
+                having count(*) > 3)
+);
 
+
+--q20
+insert into Unassigned_Faculty
+select * from faculty
+where faculty_id not in (select distinct faculty_id from courses);
