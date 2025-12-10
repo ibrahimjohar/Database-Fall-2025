3535623fdf44aa9347f962fdd61fc255517edf61
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-12-11 00:19:06 +0500

    updated lab11-tasks.sql

--- Labs/09+10+11/lab11-tasks.sql
+++ Labs/09+10+11/lab11-tasks.sql
@@ -2,43 +2,43 @@
 --lab11 - tasks
 
 --q1
-use inventoryDB
+use inventoryDB;
 
 db.furniture.insertMany([
   { name: "stool", color: "black", dimensions: [25, 40] },
   { name: "bench", color: "oak", dimensions: [55, 110] },
   { name: "cabinet", color: "white", dimensions: [45, 90] }
-])
+]);
 
 --q2
-db.furniture.insertOne({ name: "desk", color: "brown", dimensions: [50, 100] })
+db.furniture.insertOne({ name: "desk", color: "brown", dimensions: [50, 100] });
 
 --q3
-db.furniture.find({ "dimensions.0": { $gt: 30 } })
+db.furniture.find({ "dimensions.0": { $gt: 30 } });
 
 --q4
-db.furniture.find({ color: "brown", name: { $in: ["table", "chair"] } })
+db.furniture.find({ color: "brown", name: { $in: ["table", "chair"] } });
 
 --q5
-db.furniture.updateOne( { name: "table" }, { $set: { color: "ivory" } } )
+db.furniture.updateOne( { name: "table" }, { $set: { color: "ivory" } } );
 
 --q6
-db.furniture.updateMany( { color: "brown" }, { $set: { color: "dark brown" } } )
+db.furniture.updateMany( { color: "brown" }, { $set: { color: "dark brown" } } );
 
 --q7
-db.furniture.deleteOne({ name: "chair" })
+db.furniture.deleteOne({ name: "chair" });
 
 --q8
-db.furniture.deleteMany({ dimensions: [12, 18] })
+db.furniture.deleteMany({ dimensions: [12, 18] });
 
 --q9
 db.furniture.aggregate([
   {
     $group: { _id: "$color", item_count: { $sum: 1 } }
   }
-])
+]);
 
 --q10
-db.furniture.createIndex({ name: "text" })
+db.furniture.createIndex({ name: "text" });
 
-db.furniture.find({ $text: { $search: "table" } })
+db.furniture.find({ $text: { $search: "table" } });
